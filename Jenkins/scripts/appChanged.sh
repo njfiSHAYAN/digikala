9b7f5467e5da760a7b252c62742a8dce2ef16583
njfiSHAYAN <najafi.shayan4237@gmail.com>
2022-09-07 19:02:43 +0430

    bug fix

--- Jenkins/scripts/appChanged.sh
+++ Jenkins/scripts/appChanged.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 changes=$1
-appChanged=$(echo "$changes" | grep application)
+appChanged=$(echo "$changes" | tr '\n' '-' | grep application)
 if [ ${#appChanged} -gt 0 ]; then
     echo "1"
 else
--- Jenkins/scripts/deployChanged.sh
+++ Jenkins/scripts/deployChanged.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 changes=$1
-deployChange=$(echo "$changes" | grep -E "helmCharts|ansible")
+deployChange=$(echo "$changes" | tr '\n' '-' | grep -E "helmCharts|ansible")
 if [ ${#deployChange} -gt 0 ]; then
     echo "1"
 else
