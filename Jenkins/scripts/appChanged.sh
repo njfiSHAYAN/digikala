03cc9a309e2327a3fe930e41c9b2b8d475fc3c68
njfiSHAYAN <najafi.shayan4237@gmail.com>
2022-09-07 19:10:07 +0430

    bugfix2

--- Jenkins/scripts/appChanged.sh
+++ Jenkins/scripts/appChanged.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 changes=$1
-appChanged=$(echo "$changes" | tr '\n' '-' | grep application)
+appChanged=$(echo "$changes" | grep application)
 if [ ${#appChanged} -gt 0 ]; then
     echo "1"
 else
